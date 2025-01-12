0dbe984c4111efb114398bcb40b17d2629fc3854
PrasanthNandyalaa <prasanthnandyala239@gmail.com>
2025-01-12 18:41:30 +0530

    Update WelcomeController.java

--- src/main/java/com/boot/jenkins/controller/WelcomeController.java
+++ src/main/java/com/boot/jenkins/controller/WelcomeController.java
@@ -3,7 +3,7 @@ package com.boot.jenkins.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@org.springframework.web.bind.annotation.RestController
+@RestController
 public class WelcomeController {
 
 	@GetMapping("/welcome/{name}")
