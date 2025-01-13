a5456ce38e2353d384b5e4ba78a936807c3c194a
PrasanthNandyalaa <prasanthnandyala239@gmail.com>
2025-01-13 16:01:52 +0530

    Update WelcomeController.java

--- src/main/java/com/boot/jenkins/controller/WelcomeController.java
+++ src/main/java/com/boot/jenkins/controller/WelcomeController.java
@@ -10,6 +10,6 @@ public class WelcomeController {
 	@GetMapping("/welcome/{name}")
 	public String welcome(@PathVariable("name") String name) {
 		//return name+", Welcome Jenkins deployment to tomcat 10 working";
-		return name+", Welcome Jenkins deployment to tomcat 10 working after commit change to see jenkins auto trigger";
+		return name+", Welcome Jenkins deployment to tomcat 10 working after commit change to see jenkins auto trigger again";
 	}
 }
